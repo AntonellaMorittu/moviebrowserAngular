a2d19871e9f9e52de4771647796d39fce4433648
Antonella Morittu <morittuantonella@gmail.com>
2018-05-30 13:31:41 +0200

    clean info.component

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,29 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Component } from '@angular/core';
 import { SearchService } from '../search.service';
-import { Subject } from 'rxjs/Subject';
-import { Http, Response } from '@angular/http';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.css'],
-  providers: [SearchService]
+  styleUrls: ['./info.component.css']
 })
-
 export class InfoComponent implements OnInit {
-  results: Object;
+  movies: any[];
 
   constructor(private http: Http, searchService: SearchService) { }
 
   ngOnInit() {
-      this.searchService.search(this.searchMovie$)
-      .subscribe(results => {
-        console.log(results.Search);
-        this.results = results.Search;
-      });
 
   }
 
